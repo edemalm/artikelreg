61f4efa2ebb2dfc66454ce6d6fec6bb1e7d82c01
Markus Edemalm <markus@edemalm.se>
2025-07-13 16:46:07 +0200

    Updates

--- js/form-events.js
+++ js/form-events.js
@@ -3,6 +3,20 @@ $(document).ready(function() {
 	console.log('DOM ready');
 	console.log('Loading form-events.js');
 
+	// Kvalitetskontroll
+	// mdui-switch #switch-kvalitetskontroll changes
+	$('#switch-kvalitetskontroll').on('change', function() {
+		console.log('mdui-switch #switch-kvalitetskontroll changed');
+		if ($('#switch-kvalitetskontroll').prop("checked")) {
+			// is checked
+			$('#textarea-kvalitetskontrolltext').removeAttr('disabled');
+		} else {
+			// not checked
+			$('#textarea-kvalitetskontrolltext').val('');
+			$('#textarea-kvalitetskontrolltext').attr('disabled', true);
+		}
+	});
+
 	$('#button-reset-form-warning').click(function() {
 		console.log('Button #button-reset-form-warning clicked');
 		$('#dialog-reset-warning').attr('open', true);
