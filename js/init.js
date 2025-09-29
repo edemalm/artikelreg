013869beff40ca6016aa5f43b68741c761b416c6
Markus Edemalm <markus@edemalm.se>
2025-09-29 23:42:07 +0200

    Updates

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	console.log('Loading init.js');
 
 	// Global variables
-	window.update = '2025-09-28';	// Last commit date
+	window.update = '2025-09-29';	// Last commit date
 	window.debug = 'No';			// "Yes" to enable
 	window.validate_input = 'Yes';	// "Yes" to enable
 
